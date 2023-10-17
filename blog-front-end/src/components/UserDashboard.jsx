1f1818b5406c879913c422629256847e0c6a418a
steve omari <steveomari62@gmail.com>
2023-10-16 20:45:55 -0400

    format userDashboard

--- blog-front-end/src/components/UserDashboard.jsx
+++ blog-front-end/src/components/UserDashboard.jsx
@@ -4,9 +4,19 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const UserDashboard = () => {
+  const [userArticles, setUserArticles] = useState([]);
   //eslint-disable-next-line
   const navigate = useNavigate();
   const authToken = localStorage.getItem("authToken");
+
+  function decodeHTMLString(encodedHTMLString) {
+    let parser = new DOMParser();
+    let dom = parser.parseFromString(
+      "<!doctype html><body>" + encodedHTMLString,
+      "text/html"
+    );
+    return dom.body.textContent;
+  }
 
   useEffect(() => {
     if (!authToken) {
@@ -20,6 +30,32 @@ const UserDashboard = () => {
     "Content-Type": "application/json",
     Authorization: `Bearer ${authToken}`,
   };
+
+  useEffect(() => {
+    fetch("http://localhost:3000/blog/articles_user", {
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        const userArticlesData = data.map((article) => {
+          return {
+            articleId: article._id,
+            title: article.title,
+            preview: decodeHTMLString(article.preview),
+            category: article.category.name,
+            categoryId: article.category._id,
+            author: article.author.first_name,
+            authorId: article.author._id,
+            picture_decoded: decodeHTMLString(article.article_picture),
+            picture_credit: article.picture_credit,
+            article_body: decodeHTMLString(article.article_body),
+            isActive: article.article_is_active,
+          };
+        });
+        setUserArticles(userArticlesData);
+      });
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("authToken"); //remove token from client storage
@@ -35,26 +71,13 @@ const UserDashboard = () => {
     navigate("/newArtical");
   };
 
-  // Make a GET request with the token in the headers
-  //example on how to get data
-  //   fetch("http://localhost:3000/api/resource", {
-  //     method: "GET",
-  //     headers: headers,
-  //   })
-  //     .then((response) => {
-  //       // Handle the response
-  //     })
-  //     .catch((error) => {
-  //       // Handle errors
-  //     });
-
   return (
     <>
+      {console.log(userArticles)}
       <section className="section">
         <div className="title">User Dashboard</div>
 
         <div className="buttons are-medium">
-          
           <button className="button is-link" onClick={handleNewArtical}>
             <span className="icon">
               <i className="fas fa-plus"></i>
@@ -77,6 +100,58 @@ const UserDashboard = () => {
           </button>
         </div>
       </section>
+
+      <div className="section is-medium pt-4">
+        <div className="container">
+          {userArticles.map((article) => {
+            return (
+              <article key={article.articleId} className="media box">
+                <figure className="media-left">
+                  <p className="image is-64x64 is-clipped">
+                    <img src={article.picture_decoded} alt="article picture" />
+                  </p>
+                </figure>
+
+                <div className="media-content">
+                  <div className="content">
+                    <div className="title">{article.title}</div>
+
+                    {article.isActive ? (
+                      <div className="tag is-primary">Active</div>
+                    ) : (
+                      <div className="tag is-warning">Not Active</div>
+                    )}
+                    <div className="tag">{article.category}</div>
+
+                    <div className="subtitle">{article.author}</div>
+                    <div className="buttons">
+                      <button
+                        className="button is-link"
+                        onClick={""}//edit post
+                      >
+                        <span className="icon">
+                          <i className="fas fa-pencil"></i>
+                        </span>
+                        <span>Edit</span>
+                      </button>
+
+                      <button
+                        className="button is-danger"
+                        onClick={""}//delete
+                      >
+                        <span className="icon">
+                          <i className="fas fa-trash"></i>
+                        </span>
+                        <span>Delete</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
 
       <section className="section">
         <button className="button is-link is-medium" onClick={handleLogout}>
