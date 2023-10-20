8f8ab69032f4cfbd4a5b9cf35779bbde1aa14a1b
steve omari <steveomari62@gmail.com>
2023-10-20 07:11:51 -0400

    update, article update feature

--- blog-front-end/src/components/EditArticle.jsx
+++ blog-front-end/src/components/EditArticle.jsx
@@ -9,8 +9,8 @@ const tinyMCEApiKey = import.meta.env.VITE_TinyMCE_apiKey;
 import PropTypes from "prop-types";
 
 const EditArticle = ({articleToEdit}) => {
-  console.log("article to edit");
-  console.log(articleToEdit);
+  // console.log("article to edit");
+  // console.log(articleToEdit);
 //   articleId: "6513863b29028d06e37eecdb"
 // article_body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacinia quis vel eros donec ac. Leo a diam sollicitudin tempor id eu nisl. Arcu cursus euismod quis viverra nibh cras pulvinar. Neque egestas congue quisque egestas diam. Eget duis at tellus at urna condimentum mattis pellentesque id. Egestas erat imperdiet sed euismod nisi porta lorem. Porta nibh venenatis cras sed felis eget velit. Senectus et netus et malesuada fames ac turpis egestas integer. Nec ultrices dui sapien eget. Velit egestas dui id ornare arcu odio. Nibh praesent tristique magna sit. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Morbi non arcu risus quis varius quam quisque id diam. Et molestie ac feugiat sed. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse.\n\nVolutpat consequat mauris nunc congue nisi vitae suscipit tellus. Praesent tristique magna sit amet purus gravida quis. Eleifend mi in nulla posuere sollicitudin aliquam. Sed sed risus pretium quam vulputate dignissim suspendisse in est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Nisl rhoncus mattis rhoncus urna neque viverra. Arcu non sodales neque sodales ut etiam. Magnis dis parturient montes nascetur ridiculus mus. Duis convallis convallis tellus id. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. Diam volutpat commodo sed egestas. Porttitor massa id neque aliquam. Risus viverra adipiscing at in tellus integer feugiat. Tristique senectus et netus et. Sagittis id consectetur purus ut faucibus pulvinar elementum. Massa vitae tortor condimentum lacinia quis vel eros donec.\n\nEnim praesent elementum facilisis leo vel fringilla est. Eu feugiat pretium nibh ipsum consequat. Quis risus sed vulputate odio ut enim blandit volutpat maecenas. Eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar. Egestas dui id ornare arcu. Sed faucibus turpis in eu. Netus et malesuada fames ac turpis. Fermentum odio eu feugiat pretium nibh. Lectus quam id leo in vitae turpis massa sed. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Ut tristique et egestas quis ipsum suspendisse. Sed egestas egestas fringilla phasellus faucibus. Habitant morbi tristique senectus et. Nunc sed augue lacus viverra. Vestibulum morbi blandit cursus risus at ultrices mi. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Tristique risus nec feugiat in. Donec ultrices tincidunt arcu non.\n\nDignissim cras tincidunt lobortis feugiat. Leo vel fringilla est ullamcorper eget nulla. Scelerisque in dictum non consectetur a erat nam at lectus. Bibendum neque egestas congue quisque egestas diam in arcu. Dignissim enim sit amet venenatis urna cursus eget. Lacinia at quis risus sed vulputate odio. Purus sit amet volutpat consequat mauris nunc congue nisi vitae. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Platea dictumst quisque sagittis purus sit amet volutpat. Magna sit amet purus gravida quis blandit turpis cursus. Magna etiam tempor orci eu lobortis elementum. Semper feugiat nibh sed pulvinar proin. Et sollicitudin ac orci phasellus egestas. Id ornare arcu odio ut sem nulla pharetra. Semper risus in hendrerit gravida rutrum quisque. Lacus sed viverra tellus in hac. Quisque sagittis purus sit amet volutpat consequat mauris nunc. In arcu cursus euismod quis viverra nibh cras pulvinar mattis.\n\nSed vulputate mi sit amet mauris commodo quis. Sit amet est placerat in. Pellentesque habitant morbi tristique senectus et netus et. Tincidunt vitae semper quis lectus nulla at. Ante metus dictum at tempor commodo ullamcorper. In arcu cursus euismod quis viverra nibh cras pulvinar. Lectus magna fringilla urna porttitor. Arcu cursus vitae congue mauris rhoncus aenean. Volutpat sed cras ornare arcu dui vivamus arcu felis. Sit amet risus nullam eget felis eget nunc. Mauris augue neque gravida in fermentum et sollicitudin. Fringilla ut morbi tincidunt augue interdum velit. Dapibus ultrices in iaculis nunc sed. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Consequat nisl vel pretium lectus quam id."
 // author: "pilot"
@@ -33,14 +33,15 @@ const EditArticle = ({articleToEdit}) => {
   //eslint-disable-next-line
   const [formData, setFormData] = useState({
     title: articleToEdit.title,
-    // author: "",
-    category: articleToEdit.category,
+    articleId: articleToEdit.articleId,
+    author: "",
+    category: false, //articleToEdit.category,
     article_picture: articleToEdit.picture_decoded,
     picture_credit: articleToEdit.picture_credit,
     preview: articleToEdit.preview,
     article_body: articleToEdit.article_body,
     article_is_active: articleToEdit.isActive,
-    // authorId: "",
+    authorId: "",
   });
   //eslint-disable-next-line
   const navigate = useNavigate();
@@ -124,7 +125,7 @@ const EditArticle = ({articleToEdit}) => {
     const name = event.target.name;
     const value = event.target.value;
     setFormData((values) => ({ ...values, [name]: value }));
-    // console.log(formData);
+    console.log(formData);
   };
 
   const handleFormRadioChangeFalse = (event) => {
@@ -150,13 +151,11 @@ const EditArticle = ({articleToEdit}) => {
     //eslint-disable-next-line
     try {
       const response = await fetch(
-        "http://localhost:3000/blog/artical_create",
+        "http://localhost:3000/blog/article_update",
         {
           method: "POST",
           body: JSON.stringify(formData),
-          headers: {
-            "Content-Type": "application/json",
-          },
+          headers: headers,
         }
       );
 
@@ -258,7 +257,7 @@ const EditArticle = ({articleToEdit}) => {
                 /> */}
                 <div className="select">
                   <select
-                    value={formData.category || ""}
+                    value={formData.category || articleToEdit.categoryId}
                     onChange={handleCategorySelectFormChange}
                   >
                     {categories.map((category) => (
@@ -402,6 +401,7 @@ const EditArticle = ({articleToEdit}) => {
                   value={formData.article_is_active || true}
                   onClick={handleFormRadioChangeTrue}
                   type="radio"
+                  defaultChecked={formData.article_is_active === true}
                 />
                 Active Artical
               </label>
@@ -412,8 +412,8 @@ const EditArticle = ({articleToEdit}) => {
                   value={formData.article_is_active || false}
                   onClick={handleFormRadioChangeFalse}
                   type="radio"
+                  defaultChecked={formData.article_is_active === false}
                   required
-                  defaultChecked
                 />
                 Inactive Artical (Archive)
               </label>
@@ -425,7 +425,7 @@ const EditArticle = ({articleToEdit}) => {
               <p>Validation Errors</p>
               <ul>
                 {errors.map((error, index) => (
-                  <li key={index}>{error.msg}</li>
+                  <li key={index}>{error}</li>
                 ))}
               </ul>
             </div>
